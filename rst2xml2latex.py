40ebf395831f1e8434f7ee17c924a0fcf73e05fe
niryuu <niryuu@gmail.com>
2010-12-13 01:02:03 +0900

    modified

--- rst2xml2latex.py
+++ rst2xml2latex.py
@@ -3,14 +3,17 @@
 from xml.dom.minidom import parse
 import sys
 
-dom = parse(sys.argv[1])
-elements = dom.getElementsByTagName("section")
-for element in elements:
-	if element.parentNode.nodeName == "section":
-		if element.parentNode.parentNode.nodeName == "section":
-			print "subsubsection ",
+def parse_section(dom):
+	dom = parse(sys.argv[1])
+	elements = dom.getElementsByTagName("section")
+	for element in elements:
+		if element.parentNode.nodeName == "section":
+			if element.parentNode.parentNode.nodeName == "section":
+				print "subsubsection ",
+			else:
+				print "subsection ",
 		else:
-			print "subsection ",
-	else:
-		print "section ",
-	print element.getAttributeNode("names").nodeValue
+			print "section ",
+		print element.getAttributeNode("names").nodeValue
+
+dom = parse(sys.argv[1])
