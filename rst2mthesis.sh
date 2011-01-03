3f01909d851aad5a9204dce0fdf1f3567064c0e0
Tajima Itsuro <niryuu@gmail.com>
2011-01-04 02:17:45 +0900

    modified

--- rst2mthesis.sh
+++ rst2mthesis.sh
@@ -4,5 +4,4 @@ rst2xml --output-encoding=UTF-8 resume.rst resume.xml
 #rst2latex --output-encoding=UTF-8 --documentoption=11 --section-numbering resume.rst |nkf -e >resume.tex
 platex resume
 platex resume
-dvips resume
-ps2pdf resume.ps
+dvipdfmx resume
